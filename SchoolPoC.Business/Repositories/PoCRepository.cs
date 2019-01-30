557964c224c8aa3a82a11b24e4253051e94df1fb
stazwsb <alexkleszcz@wp.pl>
2019-01-30 16:40:42 +0100

    state as of before 1st meeting

--- SchoolPoC.Business/Repositories/PoCRepository.cs
+++ SchoolPoC.Business/Repositories/PoCRepository.cs
@@ -22,5 +22,7 @@ namespace SchoolPoC.Business.Repositories
 		{
 			return new List<Student>();
 		}
-	}
+
+        
+    }
 }
--- SchoolPoC/Controllers/SchoolController.cs
+++ SchoolPoC/Controllers/SchoolController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using SchoolPoC.DAL.Entities;
 
 namespace SchoolPoC.Controllers
 {
@@ -10,7 +13,14 @@ namespace SchoolPoC.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:53529/api/");
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            IEnumerable<Teacher> teachList;
+            HttpResponseMessage response = client.GetAsync("Teachers").Result;
+            teachList = response.Content.ReadAsAsync<IEnumerable<Teacher>>().Result;
+            return View(teachList);
         }
     }
 }
